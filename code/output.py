b78d7eb2aa4a3a0a37bda693a8c37f5d4998f5ca
pompoen1 <samson-ik@hotmail.com>
2021-01-07 12:10:23 +0100

    beginning

--- code/output.py
+++ code/output.py
@@ -3,9 +3,10 @@ from matplotlib.ticker import MultipleLocator
 import csv
 
 
-with open(r"example/output.csv") as f:
-    for i in f.readline():
-        print(i)
+with open(r"../example/output.csv") as f:
+    for line in f:
+        print(line)
+    
 
 
 fig = plt.figure()
