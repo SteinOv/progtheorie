ca6eaefeb41341118f10057d3c5a6bffad965f60
pompoen1 <samson-ik@hotmail.com>
2021-01-08 11:44:09 +0100

    adjusted main.py

--- code/main.py
+++ code/main.py
@@ -9,6 +9,8 @@ def main():
         print("Usage: python3 main.py <chip_id> <netlist_id> ")
         exit(1)
 
+    chip_id, netlist_id = argv[1:]
+
     chip_file = f"../data/chip_{chip_id}/print_{chip_id}.csv"
     netlist_file = f"../data/chip_{chip_id}/netlist_{netlist_id}.csv"
 
@@ -22,15 +24,15 @@ def main():
 
         # write net connections and routes
         for net in board.nets:
-            writer.writerow([str(net.connect), str(net.route))
+            writer.writerow([str(net.connect), str(net.route)])
 
-        writer.writerow([f"chip_{chip_id}_net_{netlist_id}", board.cost)
+        writer.writerow([f"chip_{chip_id}_net_{netlist_id}", board.cost])
 
         
 
 
 
-if __name__ is __main__:
+if __name__ == '__main__':
     main()
 
 
