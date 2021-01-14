292ac7c88455c8af25cad95a6d528225335aa020
Anoukhooijschuur <60708470+Anoukhooijschuur@users.noreply.github.com>
2021-01-14 13:08:20 +0100

    User prompts for algorithm and solutions

--- code/main.py
+++ code/main.py
@@ -4,20 +4,33 @@ from visualization.output_3D import plot_output3D
 from visualization.output_2D import plot_output2D
 import csv
 from importlib import import_module
+import os
 
 
 def main():
     """run the main program"""
     # ensure proper usage
-    if not (len(argv) in [3, 4]):
-        print("Usage: python3 main.py <chip_id> <netlist_id> <algorithm (optional)> ")
+    if not (len(argv) in [2, 3]):
+        print("Usage: python3 main.py <chip_id> <netlist_id> ")
         exit(1)
 
-    # set algorithm to default if none is given
-    if len(argv) == 4:
-        algorithm = argv[3]
-    else:
-        algorithm = "basic"
+    algorithms = []
+
+    for filename in os.listdir('algorithms/'):
+        if filename.endswith(".py"):
+            algorithms.append(filename[:-3])
+
+    # prompt user for desired algorithm
+    print("Which algorithm would you like to run?")
+    print(algorithms)
+    command = input("> ")
+
+    if command in algorithms:
+        algorithm = str(command)
+
+    # prompt user for desired number of solutions
+    print("How many solutions do you want to generate?")
+    n_solutions = int(input("> "))
 
 
     # TODO
@@ -36,33 +49,33 @@ def main():
     # Run algorithm
     alg = import_module(f"algorithms.{algorithm}")
     alg_func = getattr(alg, algorithm)
-    alg_func(board)
-
-
-
-    # TODO
-    output_file = "output.csv"
-
-
-    with open(f"{folder}/{output_file}", "w") as file:
-        writer = csv.writer(file)
-
-        # write header row
-        writer.writerow(["net", "wires"])
-
-        # write net connections and routes
-        for net in board.nets:
-            writer.writerow([str(net.connect), str(net.route)])
+    
+    for i in range(n_solutions):
+        alg_func(board)
 
         # TODO
-        writer.writerow([f"chip_{chip_id}_net_{netlist_id}", board.cost])
+        output_file = "output.csv"
 
-    # create output plot
-    if board.height > 0:
-        plot_output3D(output_file, folder, board)
-    else:
-        plot_output2D(output_file, folder)
-    
+
+        with open(f"{folder}/{output_file}", "w") as file:
+            writer = csv.writer(file)
+
+            # write header row
+            writer.writerow(["net", "wires"])
+
+            # write net connections and routes
+            for net in board.nets:
+                writer.writerow([str(net.connect), str(net.route)])
+
+            # TODO
+            writer.writerow([f"chip_{chip_id}_net_{netlist_id}", board.cost])
+
+        # create output plot
+        if board.height > 0:
+            plot_output3D(output_file, folder, board)
+        else:
+            plot_output2D(output_file, folder)
+        
 
 
 if __name__ == "__main__":
